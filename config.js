c4c67ee8faf6230d3267142545536badb46be2e3
CaptainHackerGuy <97624039+CaptainHackerGuy@users.noreply.github.com>
2024-09-17 20:04:02 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+// Firebase configuration constants
+export const FIREBASE_API_KEY = "AIzaSyAIPBGFVvGhQCaEj_kBiHi4BqjClaQgfLY";
+export const FIREBASE_AUTH_DOMAIN = "webserver-c9057.firebaseapp.com";
+export const FIREBASE_DATABASE_URL = "https://webserver-c9057-default-rtdb.firebaseio.com";
+export const FIREBASE_PROJECT_ID = "webserver-c9057";
+export const FIREBASE_STORAGE_BUCKET = "webserver-c9057.appspot.com";
+export const FIREBASE_MESSAGING_SENDER_ID = "907721642809";
+export const FIREBASE_APP_ID = "1:907721642809:web:e2c6e92b828979fe2a8ee8";
